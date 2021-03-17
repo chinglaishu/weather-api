dab9c99ec192a1ad8105cab021f0316ea86e0ba3
david.lai <chinalaishu@gmail.com>
2021-03-17 17:53:55 +0800

    weather api

--- src/app.ts
+++ src/app.ts
@@ -7,23 +7,16 @@ import config from "../config/config";
 
 const dbURI = config.dbURI;
 
-if (process.env.APP_ENV !== "test") {
-  mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => console.log("connect to mongodb"))
-    .catch((err: Error) => console.log(`fail connect to mongodb: ${err}`));
-}
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log("connect to mongodb"))
+  .catch((err: Error) => console.log(`fail connect to mongodb: ${err}`));
 
 const app = express();
 app.use(cors());
 app.use((responseTime()));
 app.use(routes);
 
-if (process.env.APP_ENV !== "test") {
-  app.listen(3000);
-  console.log("run server");
-} else {
-  app.enable('trust proxy');
-}
-
+app.listen(3000);
+console.log("run server");
 
 export default app;
