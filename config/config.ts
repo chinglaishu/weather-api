081c713e0d655c87bb5a93df32684aa61283721c
david.lai <chinalaishu@gmail.com>
2021-03-17 18:59:05 +0800

    add type and required to schema

--- config/config.ts
+++ config/config.ts
@@ -1,9 +1,13 @@
 
-const config = {
+const dbUser = `testuser`;
+// should be in process.env
+const dbPassword = `AyYTzKVAQ6pbHHN8`;
+
+const config: any = {
   getWeatherForecastURI(lang: string) {
     return `https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=fnd&lang=${lang}`;
   },
-  dbURI: "mongodb+srv://testuser:AyYTzKVAQ6pbHHN8@weather.3zse3.mongodb.net/weather-data?retryWrites=true&w=majority",
+  dbURI: `mongodb+srv://${dbUser}:${dbPassword}@weather.3zse3.mongodb.net/weather-data?retryWrites=true&w=majority`,
 };
 
 export default config;
--- models/WeatherForecast.ts
+++ models/WeatherForecast.ts
@@ -2,12 +2,51 @@ import mongoose from "mongoose";
 
 const {Schema} = mongoose;
 
+const ForecastDataDetail = {
+  value: Number,
+  unit: String,
+};
+
+const ForecastData = {
+  forecastDate: String,
+  week: String,
+  forecastWind: String,
+  forecastWeather: String,
+  forecastMaxtemp: ForecastDataDetail,
+  forecastMintemp: ForecastDataDetail,
+  forecastMaxrh: ForecastDataDetail,
+  forecastMinrh: ForecastDataDetail,
+  ForecastIcon: Number,
+};
+
+const TempData = {
+  place: String,
+  value: Number,
+  unit: String,
+  recordTime: String,
+};
+
 const WeatherForecastSchema = new Schema({
-  generalSituation: String,
-  lagn: String,
-  weatherForecast: [],
-  seaTemp: {},
-  soilTemp: []
+  generalSituation: {
+    type: String,
+    required: true,
+  },
+  lang: {
+    type: String,
+    required: true,
+  },
+  weatherForecast: {
+    type: [ForecastData],
+    required: true,
+  },
+  seaTemp: {
+    type: TempData,
+    required: true,
+  },
+  soilTemp: {
+    type: [TempData],
+    required: true,
+  },
 }, {timestamps: {}});
 
 const WeatherForecast = mongoose.model("WeatherForecast", WeatherForecastSchema);
